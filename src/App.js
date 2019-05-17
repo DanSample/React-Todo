2f081b3356d5729f958c135aebdf013bb741dc8c
Dan Sample <danssample@gmail.com>
2019-05-16 20:34:38 -0700

    built out App class constructor with state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,63 @@
 import React from 'react';
+import Todo from './components/TodoComponents/Todo';
+import TodoForm from './components/TodoComponents/TodoForm';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          task: 'Organize Garage',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ],
+      todo: ''
+    };
+  }
+
+  addTask = event => {
+    event.preventDefault(); //This will prevent the entire page reloading and only the affected component.
+    const newTask = { task: this.state.todo, completed: false, id: Date.now() };
+    this.setState({
+      todos: [...this.state.todos, newTask],
+      todo: ''
+    });
+  };
+
+  changeTodo = event =>
+    this.setState({ [event.target.name]: event.target.value });
+
+  toggleTaskComplete = id => {
+    let todos = this.state.todos.slice();
+    todos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+        return todo;
+      } else {
+        return todo;
+      }
+    });
+    this.setState({ todos });
+  };
+
+  clear;
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <Todo />
+        <TodoForm />
       </div>
     );
   }
