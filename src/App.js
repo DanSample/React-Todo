b375840cb22ba7302c81cceb86399b02b34b957c
Dan Sample <danssample@gmail.com>
2019-05-16 20:51:54 -0700

    built out Todo.js and imported it to TodoList.js

--- src/App.js
+++ src/App.js
@@ -22,32 +22,14 @@ class App extends React.Component {
     };
   }
 
-  addTask = event => {
-    event.preventDefault(); //This will prevent the entire page reloading and only the affected component.
-    const newTask = { task: this.state.todo, completed: false, id: Date.now() };
+  addTask = e => {
+    e.preventDefault(); //This will prevent the re-rendering of the entire page and will only re-render the component.
+    const newTask = {task: this.state.todo, completed: false, id: Date.now()};
     this.setState({
       todos: [...this.state.todos, newTask],
       todo: ''
-    });
-  };
-
-  changeTodo = event =>
-    this.setState({ [event.target.name]: event.target.value });
-
-  toggleTaskComplete = id => {
-    let todos = this.state.todos.slice();
-    todos = todos.map(todo => {
-      if (todo.id === id) {
-        todo.completed = !todo.completed;
-        return todo;
-      } else {
-        return todo;
-      }
-    });
-    this.setState({ todos });
-  };
-
-  clear;
+    })
+  
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
@@ -56,7 +38,7 @@ class App extends React.Component {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
-        <Todo />
+        <TodoList />
         <TodoForm />
       </div>
     );
